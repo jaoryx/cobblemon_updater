8288d6a0707ab1d8550cba68293a632768714d15
JaoryxHowest <jordan.baveghems@student.howest.be>
2025-01-22 11:17:04 +0100

    Update UI for resourcepacks

--- Cobblemon.Updater.Core/obj/Debug/net8.0/Cobblemon.Updater.Core.AssemblyInfo.cs
+++ Cobblemon.Updater.Core/obj/Debug/net8.0/Cobblemon.Updater.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cobblemon.Updater.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ae8f0fc217f93bbef7174ad6440b1e7cc25c259")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cobblemon.Updater.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cobblemon.Updater.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
